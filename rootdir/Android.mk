ec14867db09054693a31dda6d2755698e6de1a1a
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-19 08:32:12 -0700

    wt88047: rootdir: Move hardware specific .rc files to /vendor/etc/init/hw
    The reason these files are moved to /vendor/etc/init/hw and
    not /vendor/etc/init is because 'init' scans the /vendor/etc/init folder
    for vendor service .rc fragments to import. That would either result
    in duplicate imports or out-of-order imports of these hardware
    specific .rc files and that *may* have unintended consequences.
    
    In order to preserve the current import order for MTP, we move all
    hardware specific .rc files to /vendor/etc/init/hw which is
    autoscanned by init.
    
    Change-Id: I5e7664dc4c652b2295042e507eb4eec0e06c16c9
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -76,21 +76,12 @@ LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
-# Copy the power config for recovery too
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.power.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -99,5 +90,5 @@ LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/init/hw
 include $(BUILD_PREBUILT)
