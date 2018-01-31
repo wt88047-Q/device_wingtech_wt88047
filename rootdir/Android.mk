6e44420e6c0a5ce4a210126fb4791c987ee9cacb
LuK1337 <priv.luk@gmail.com>
2018-01-31 12:47:59 +0100

    wt88047: rootdir: Move fstab to /vendor
    Change-Id: I2e86c3be08d0d4f19a6cf89a931f4a1e6ac12cdf

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,6 +43,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
