f69accd1250ad315da6a4dfc22fb067f1e5569c7
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 18:34:18 +0430

    wt88047: Allow broken PHONY targets
    Change-Id: Idb8418cfb6335fa7257e084e1d135dc6fb29ebc4
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ DEVICE_PATH := device/wingtech/wt88047
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PHONY_TARGETS := true
 
 # Architecture
 ifneq ($(FORCE_32_BIT),true)
