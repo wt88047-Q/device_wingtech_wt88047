9937e9c7e1e8fa348b61d19a5cf13eed2a90462b
Alessandro Astone <ales.astone@gmail.com>
2019-09-05 22:17:57 +0200

    wt88047: Create /persist and /firmware mountpoints via build system
     * With System As Root / is a read-only filesystem
       so we can't create our mountpoints in init anymore
    
    Change-Id: I513b5d47ec0703aef65065a7ff26d5ad941384e7

---
+++ config.fs
@@ -0,0 +1,32 @@
+[AID_VENDOR_QCOM_DIAG]
+value:2950
+
+[AID_VENDOR_RFS]
+value:2951
+
+[AID_VENDOR_RFS_SHARED]
+value:2952
+
+[vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
