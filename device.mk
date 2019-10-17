528d8a680c62e6354ba5db48cb4cea069def11f7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-10-18 01:13:01 +0530

    wt88047: Add IMS packages in powersave whitelist
     * Prevent VoLTE from vanishing after waking up from deep sleep when
       Aggressive Doze is enabled
    
    Change-Id: I5f9d2e558eb039f1fbc92a3ff5b190d04bf99f2a

--- device.mk
+++ device.mk
@@ -392,6 +392,9 @@ PRODUCT_BOOT_JARS += \
     ims-ext-common_system \
     telephony-ext
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Thermal
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
